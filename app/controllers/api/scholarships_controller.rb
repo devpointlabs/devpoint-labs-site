e0c5aa5a001c2c48b81b83777f288c7c7ad73c78
Nathan Swift <nswiftnswift@gmail.com>
2019-04-01 16:49:52 -0600

    ⏣ built scholarship controller

---
+++ app/controllers/api/scholarships_controller.rb
@@ -0,0 +1,34 @@
+class Api::ScholarshipsController < ApplicationController
+  before_action :set_scholarship, only: [:show, :update, :destroy]
+
+  def index
+    render json: Scholarship.all
+  end
+
+  def show
+    render json: @scholarship
+  end
+
+  def create
+    scholarship = Scholarship.new(scholarship_params)
+    if scholarship.save
+      render json: scholarship
+    else
+      render json: scholarship.errors, status: 422  
+    end
+  end
+
+  def destroy
+    @scholarship.destroy
+    render json: { message: "scholarship #{@scholarship.name} was deleted" }
+  end
+
+  private
+    def set_scholarship
+      @scholarship = Scholarship.find(params[:id])  
+    end
+
+    def scholarship_params
+      params.require(:scholarship).permit(:name)  
+    end
+end
