39b32a18fdf61a436b2c09a9bff3031e2509004c
Ryan Gale <ryanwfgale@gmail.com>
2019-04-01 16:57:44 -0600

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,23 @@ import NoMatch from './components/NoMatch';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
 import Register from './components/Register';
+import FetchUser from './components/FetchUser'
 import { Switch, Route, } from 'react-router-dom';
 import { Container, } from "semantic-ui-react";
 
 const App = () => (
   <Fragment>
     <Navbar />
-    <Container>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-        <Route component={NoMatch} />
-      </Switch>
-    </Container>
+    <FetchUser>
+      <Container>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Container>
+    </FetchUser>
   </Fragment>
 )
 
