4a097b4a2c45b1762729a5414d084b9063c1ae45
Ryan Gale <ryanwfgale@gmail.com>
2019-04-01 15:31:11 -0600

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Fragment, } from 'react';
+import Home from './components/Home';
+import NoMatch from './components/NoMatch';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Register from './components/Register';
+import { Switch, Route, } from 'react-router-dom';
+import { Container, } from "semantic-ui-react";
+
+const App = () => (
+  <Fragment>
+    <Navbar />
+    <Container>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Container>
+  </Fragment>
+)
+
+export default App;
+
---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import { BrowserRouter, } from 'react-router-dom';
+import { AuthProvider, } from "./providers/AuthProvider";
+import 'semantic-ui-css/semantic.min.css';
+
+ReactDOM.render(
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>,
+  document.getElementById('root')
+);
+
+
