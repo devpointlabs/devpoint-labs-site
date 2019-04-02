d2fa8f908a9b13419ef2ab2ec67a706efbc3a61c
Ryan Gale <ryanwfgale@gmail.com>
2019-04-02 17:23:49 -0600

    add basic contact form

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
-import React, { Fragment, } from 'react';
-import Home from './components/Home';
-import NoMatch from './components/NoMatch';
-import Courses from './components/Courses';
-import About from './components/About';
-import Community from './components/Community';
-import Application from './components/Application';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Login from './components/Login';
-import Register from './components/Register';
-import { Switch, Route, } from 'react-router-dom';
-import { Container, } from "semantic-ui-react";
-import FetchUser from './components/FetchUser';
-
+import React, { Fragment } from "react";
+import Home from "./components/Home";
+import NoMatch from "./components/NoMatch";
+import Courses from "./components/Courses";
+import About from "./components/About";
+import Community from "./components/Community";
+import Application from "./components/Application";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import ContactForm from "./components/ContactForm";
+import { Switch, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+import FetchUser from "./components/FetchUser";
 
 const App = () => (
   <Fragment>
@@ -27,13 +27,13 @@ const App = () => (
           <Route exact path="/About" component={About} />
           <Route exact path="/Community" component={Community} />
           <Route exact path="/Application" component={Application} />
+          <Route exact path="/ContactUs" component={ContactForm} />
           <Route component={NoMatch} />
         </Switch>
-      <Footer />
+        <Footer />
       </Container>
     </FetchUser>
   </Fragment>
-)
+);
 
 export default App;
-
