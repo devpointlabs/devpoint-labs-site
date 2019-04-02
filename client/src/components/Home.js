619f78cd4f387b477efa332906860fab764ec4a1
Spencer Harmon <spencer.harmon@gmail.com>
2019-04-02 14:06:59 -0600

    IDos component added, not finished

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,41 +1,17 @@
 import React from 'react';
+import IDos from './IDos'
 import { Button, Divider, Grid, Header, Icon, Search, Segment } from 'semantic-ui-react'
 import styled from 'styled-components'
 
 class Home extends React.Component {
 
-  render () {
+  render() {
     return (
-      <Segment placeholder>
-        <Grid columns={2} stackable textAlign='center'>
-        <Divider vertical>Or</Divider>
-
-        <Grid.Row verticalAlign='middle'>
-          <Grid.Column>
-            <Header icon>
-              <Icon name='search' />
-              Find Country
-            </Header>
-
-            <Search placeholder='Search countries...' />
-          </Grid.Column>
-
-          <Grid.Column>
-            <Header icon>
-              <Icon name='world' />
-              Add New Country
-            </Header>
-            <Button primary>Create</Button>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
+      <IDos/>
     )
   }
 }
 
-const SegmentStyle = styled(Segment)`
-  width: 100%
-`
+
 
 export default Home
