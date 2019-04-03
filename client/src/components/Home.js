4c630bcba98c81da6b2bedaa06931e7bf844bc45
Ryan Gale <ryanwfgale@gmail.com>
2019-04-03 17:33:27 -0600

    add container to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import IDos from './IDos'
-import { Button, Divider, Grid, Header, Icon, Search, Segment } from 'semantic-ui-react'
-import styled from 'styled-components'
-import PageOptions from './PageOptions'
-import Landing from './Landing'
-import Reviews from './Reviews';
-import CoursesSection from './CoursesSection';
-import CampusTour from './CampusTour';
+import React from "react";
+import IDos from "./IDos";
+import { Container } from "semantic-ui-react";
+import styled from "styled-components";
+import PageOptions from "./PageOptions";
+import Landing from "./Landing";
+import Reviews from "./Reviews";
+import CoursesSection from "./CoursesSection";
+import CampusTour from "./CampusTour";
 
 class Home extends React.Component {
-
   render() {
     return (
       <>
-        <Landing />
-        <PageOptions />
-        <IDos/>
-        <Reviews />
-        <CampusTour />
-        <CoursesSection />
+        <Container>
+          <Landing />
+          <PageOptions />
+          <IDos />
+          <Reviews />
+          <CampusTour />
+          <CoursesSection />
+        </Container>
       </>
-    )
+    );
   }
 }
 
-
-
-export default Home
+export default Home;
