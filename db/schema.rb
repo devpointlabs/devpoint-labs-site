4e43383cd9de16b2f2d052989f45d5a40c9a021d
Thanh Bui <ttthanhbui@gmail.com>
2019-04-04 09:51:47 -0600

    subscribes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_03_204232) do
+ActiveRecord::Schema.define(version: 2019_04_03_214905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,12 @@ ActiveRecord::Schema.define(version: 2019_04_03_204232) do
     t.string "email"
     t.string "phone_number"
     t.string "currenty_city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscribes", force: :cascade do |t|
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
