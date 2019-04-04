12785f04041922112f1de2ea9b04b306be63eae1
Nathan Swift <nswiftnswift@gmail.com>
2019-04-04 14:46:04 -0600

    init mobilenav build

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { AuthConsumer, } from "../providers/AuthProvider"
-import { Menu, Dropdown, } from 'semantic-ui-react'
+import { Menu, Dropdown, Icon, Button } from 'semantic-ui-react'
 import { Link, withRouter, } from 'react-router-dom'
+import styled from 'styled-components'
+import media from '../theme/media'
 
 
 class Navbar extends React.Component {
+
+  state = {
+    show: false
+  }
 
   rightNavItems = () => {
     const { auth: { user, handleLogout, }, location, } = this.props;
@@ -33,7 +39,89 @@ class Navbar extends React.Component {
     }
   }
 
+  handleDropdown = () => {
+    const { show } = this.state
 
+    if (show)
+      return(
+        <Menu vertical>
+          <Menu.Item>
+            <Menu.Header>Products</Menu.Header>
+
+            <Menu.Menu>
+              <Menu.Item
+                name='enterprise'
+                active={this.activeItem === 'enterprise'}
+                onClick={this.handleItemClick}
+              />
+              <Menu.Item
+                name='consumer'
+                active={this.activeItem === 'consumer'}
+                onClick={this.handleItemClick}
+              />
+            </Menu.Menu>
+          </Menu.Item>
+
+          <Menu.Item>
+            <Menu.Header>CMS Solutions</Menu.Header>
+
+            <Menu.Menu>
+              <Menu.Item
+                name='rails'
+                active={this.activeItem === 'rails'}
+                onClick={this.handleItemClick}
+              />
+              <Menu.Item
+                name='python'
+                active={this.activeItem === 'python'}
+                onClick={this.handleItemClick}
+              />
+              <Menu.Item name='php' active={this.activeItem === 'php'} onClick={this.handleItemClick} />
+            </Menu.Menu>
+          </Menu.Item>
+
+          <Menu.Item>
+            <Menu.Header>Hosting</Menu.Header>
+
+            <Menu.Menu>
+              <Menu.Item
+                name='shared'
+                active={this.activeItem === 'shared'}
+                onClick={this.handleItemClick}
+              />
+              <Menu.Item
+                name='dedicated'
+                active={this.activeItem === 'dedicated'}
+                onClick={this.handleItemClick}
+              />
+            </Menu.Menu>
+          </Menu.Item>
+
+          <Menu.Item>
+            <Menu.Header>Support</Menu.Header>
+
+            <Menu.Menu>
+              <Menu.Item name='email' active={this.activeItem === 'email'} onClick={this.handleItemClick}>
+                E-mail Support
+              </Menu.Item>
+
+              <Menu.Item name='faq' active={this.activeItem === 'faq'} onClick={this.handleItemClick}>
+                FAQs
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu.Item>
+        </Menu>
+    ) 
+    else {
+      return (
+        null
+      )
+    }
+  }
+
+  toggleClick = () => this.setState({ show: !this.state.show })
+
+  handleItemClick = name => this.setState({ activeItem: name })
 
   render() {
 
@@ -59,7 +147,7 @@ class Navbar extends React.Component {
               active={this.props.location.pathname === '/'}
             />
           </Link>
-          <Menu.Menu position='right'>
+          <MenuWeb position='right'>
             <Menu.Item>
               <Dropdown text='Courses' options={courseOptions} simple item />
             </Menu.Item>
@@ -82,12 +170,47 @@ class Navbar extends React.Component {
                 active={this.props.location.pathname === '/register'}
               />
             </Link>
+          </MenuWeb>
+
+          <MenuMobile pointing secondary> 
+            <Menu.Menu position='right'>
+              <Menu.Item >
+                <Button 
+                  icon
+                  onClick={this.toggleClick}
+                  >
+                  <Icon 
+                  name='align justify'
+                  >
+                  </Icon>
+                </Button>
+              </Menu.Item>
+              { this.handleDropdown() }
           </Menu.Menu>
+          </MenuMobile>
+
         </Menu>
       </div>
     )
   }
 }
+
+const MenuMobile = styled(Menu.Menu)`
+  display: none !important
+
+  ${media.phone`
+    display: flex !important
+    float: right !important
+  `}
+`
+
+const MenuWeb = styled(Menu.Menu)`
+  display: flex
+
+  ${media.phone`
+    display: none !important
+  `}
+`
 
 export class ConnectedNavbar extends React.Component {
   render() {
