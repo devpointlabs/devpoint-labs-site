0f9160fab8b57783f60c1bf34b9507b32b0b480d
Beau Hicken <Turbodiesel2500dee@hotmail.com>
2019-04-05 14:21:08 -0600

    made components for part time las vegas

--- client/src/components/PartTimeLV.js
+++ client/src/components/PartTimeLV.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Topimages from '../partimeLVcomponents/Topimages';
+import StudentSched2 from '../partimeLVcomponents/StudentSched2';
+import Housing from '../partimeLVcomponents/Housing'
+import Costs from '../partimeLVcomponents/Costs'
 
 
 const PartTimeLV = () => {
     return(
-        <div> Las Vegas Part-Time</div>
+        <>
+        <div>Part time Las Vegas Page</div>
+        <Topimages />
+        <StudentSched2 />
+        <Housing />
+        <Costs />
+        </>
 
     )
 }
