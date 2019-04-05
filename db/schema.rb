609f2a055fdfa97250e046b53a57f354b0a6bf2d
Thanh Bui <ttthanhbui@gmail.com>
2019-04-05 14:07:30 -0600

    stackable reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_05_012156) do
+ActiveRecord::Schema.define(version: 2019_04_04_205029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,15 +44,6 @@ ActiveRecord::Schema.define(version: 2019_04_05_012156) do
     t.datetime "updated_at", null: false
     t.string "course"
     t.string "location"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "scholarships", force: :cascade do |t|
@@ -106,5 +97,4 @@ ActiveRecord::Schema.define(version: 2019_04_05_012156) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
-  add_foreign_key "posts", "users"
 end
