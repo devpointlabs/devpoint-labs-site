36f1f7de16158af44ca53b6aa3b6bb2b30ce67e4
Thanh Bui <ttthanhbui@gmail.com>
2019-04-10 14:02:57 -0600

    Instagram sizes

--- client/src/components/Instagram.js
+++ client/src/components/Instagram.js
@@ -1,23 +1,16 @@
 import React from 'react'
-// import axios from 'axios'
 import request from 'superagent'
-import { List, Image, Grid, Segment } from 'semantic-ui-react'
+import { Image, Grid, Segment, Container, Header, Icon  } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 class Instagram extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       photos: [],
-      count: 6
     }
   }
-
-  // state = { photos: [], }
-
-  // componentDidMount() {
-  //   axios.get('/api/instagrams')
-  //     .then( res => this.setState({ photos: res.data }))
-  // }
 
   componentWillMount() {
     this.fetchPhotos()
@@ -34,24 +27,110 @@ class Instagram extends React.Component {
   }
 
   render() {
+    const { photos } = this.state
     return (
-      <Segment>
-        <Grid columns={4}>
-          <Grid.Row>
-        {this.state.photos.slice(0, 6).map( photo => (
-          <Grid.Column key={photo.id}>
-          <Image src={photo.images.standard_resolution.url} />
-          </Grid.Column>
-        ))}
-        </Grid.Row>
+      <Segment style={{ background: 'gainsboro' }}>
+      <GridStyle>
+        <Grid stackable columns={4}>
+          <Grid.Row style={styles.row}>
+            {photos.slice(0, 6).map(photo => (
+              <Grid.Column key={photo.id}>
+                <Image src={photo.images.standard_resolution.url} />
+              </Grid.Column>
+            ))}
+            {/* <div style={styles.div}>
+            <Segment style={styles.seg}>
+            <Grid stackable columns={2} style={{margin: '1em'}}>
+            <Grid.Column width={9}>
+            <p>Tagline</p>
+            <Header style={{fontSize: '3em'}}>You're in good company</Header>
+            <p>When applied to building block a website or similar work product, 
+            a Visual Guide can be an intermediate step toward.</p>
+            </Grid.Column>
+            <Grid.Column width={7}>
+            <a href='https://www.instagram.com/devpointlabs/?hl=en' target='_blank'>
+            <Icon name='instagram' size='big' style={{float: 'right'}} />
+            </a>
+            <a href='https://www.facebook.com/DevPointLabs/' target='_blank'>
+            <Icon name='facebook' size='big' style={{float: 'right'}} />
+            </a>
+            </Grid.Column>
+            </Grid>
+            </Segment>
+          </div> */}
+          </Grid.Row>
         </Grid>
+          </GridStyle>
+        {/* <Link to='/'>
+          <p style={styles.p}>Learn More ></p>
+        </Link> */}
       </Segment>
     )
   }
 }
 
-export default Instagram
+const GridStyle = styled.div`
+  @media (max-width: 2560px) {
+    paddingTop: 2em,
+    paddingLeft: 9em,
+    paddingRight: 7em  
+  }
+  
+  @media (max-width: 1500px) {
+    paddingTop: 2em,
+    paddingLeft: 9em,
+    paddingRight: 7em  
+  }
+  
+  @media (max-width: 1030px) {
+    paddingTop: 2em,
+    paddingLeft: 9em,
+    paddingRight: 7em  
+  }
+  
+  @media (max-width: 780px) {
+    paddingTop: 2em,
+    paddingLeft: 9em,
+    paddingRight: 7em  
+  }
+  
+  @media (max-width: 450px) {
+    paddingTop: 2em,
+    paddingLeft: 9em,
+    paddingRight: 7em  
+  }
+  
+  @media (max-width: 380px) {
+    paddingTop: 2em,
+    paddingLeft: 9em,
+    paddingRight: 7em  
+  }
+  
+`
 
-{/* <Grid.Column key={photo.id}>
-  <Image src={photo.images.standard_resolution.url} />
-</Grid.Column> */}
+const styles = {
+  row: {
+    display: 'flex',
+    flexDirection: 'row-reverse',
+  },
+  // grid: {
+  //   paddingTop: '2em',
+  //   paddingLeft: '9em',
+  //   paddingRight: '7em',
+  // },
+  // p: {
+  //   textAlign: 'center',
+  //   padding: '3.5em',
+  // },
+  // div: {
+  //   width: '50%',
+  //   height: '50%',
+  // },
+  // seg: {
+  //   margin: '1em',
+  //   height: '24em',
+  //   background: 'lightblue'
+  // }
+}
+
+export default Instagram
